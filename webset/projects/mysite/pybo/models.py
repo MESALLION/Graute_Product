cebd37c74b8440925d306566c2fde7bcee68666c
MESALLION <MESALLION@gmail.com>
2024-04-15 20:34:49 +0900

    재수정중

--- webset/projects/mysite/pybo/models.py
+++ webset/projects/mysite/pybo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Detection(models.Model):
+    image = models.ImageField(upload_to='detection/')
+    detected_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
--- webset/projects/mysite/pybo/views.py
+++ webset/projects/mysite/pybo/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Detection
+from django.views.decorators.csrf import csrf_exempt
+import datetime
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+@csrf_exempt
+def receive_detection(request):
+    if request.method == "POST":
+        # 파일과 데이터를 받습니다.
+        image = request.FILES.get('image')
+        description = request.POST.get('description', '')
+
+        # 모델에 저장합니다.
+        detection = Detection.objects.create(
+            image=image,
+            description=description
+        )
+
+        return JsonResponse({"success": True, "detected_at": detection.detected_at}, status=201)
+    return JsonResponse({"success": False, "error": "Invalid request"}, status=400)
